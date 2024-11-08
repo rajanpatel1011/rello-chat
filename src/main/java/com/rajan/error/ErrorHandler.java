3168610d8ab7a654f9c58be7182af040adad6223
Rajan <rajanpatel1011@gmail.com>
2024-11-08 08:43:34 -0500

    add back js scr

--- src/main/java/com/rajan/error/ErrorHandler.java
+++ src/main/java/com/rajan/error/ErrorHandler.java
@@ -1,6 +1,5 @@
 package com.rajan.error;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.StompSubProtocolErrorHandler;
 
 public class ErrorHandler extends StompSubProtocolErrorHandler {
